e2d99edd0a9bafcc9944cf704501edf91b3505d3
Abel mekonn <123172904+Abelmekonn@users.noreply.github.com>
2024-06-25 23:45:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import pandas as pd
 # Load the dataset
 data = pd.read_csv('student-data.csv')
 
-# Display the first few rows of the dataset
+# Display the first few rows of data
 print(data.head())
